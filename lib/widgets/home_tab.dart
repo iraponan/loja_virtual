63cc0495c66ea521f51b72bf46f8bb5633e6b471
Iraponan Marinho <iraponan.aires@gmail.com>
2024-02-07 00:17:09 -0300

    Grade de Imagens.

--- lib/widgets/home_tab.dart
+++ lib/widgets/home_tab.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -23,7 +25,10 @@ class HomeTab extends StatelessWidget {
               ),
             ),
             FutureBuilder<QuerySnapshot>(
-              future: FirebaseFirestore.instance.collection('home').orderBy('pos').get(),
+              future: FirebaseFirestore.instance
+                  .collection('home')
+                  .orderBy('pos')
+                  .get(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
                   return SliverToBoxAdapter(
@@ -36,12 +41,22 @@ class HomeTab extends StatelessWidget {
                     ),
                   );
                 } else {
-                  print('Tamanho da coleção. = ${snapshot.data?.docs.length}');
                   return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200,
-                      alignment: Alignment.center,
-                      child: Container(),
+                    child: StaggeredGrid.count(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 1.0,
+                      crossAxisSpacing: 1.0,
+                      children: snapshot.data!.docs.map((doc) {
+                        return StaggeredGridTile.count(
+                          crossAxisCellCount: doc['x'],
+                          mainAxisCellCount: doc['y'],
+                          child: FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: doc['image'],
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      }).toList(),
                     ),
                   );
                 }
