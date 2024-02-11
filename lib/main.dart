42c15ee7af0e4f55d726b5290aeb738bb4fb4c82
Iraponan Marinho <iraponan.aires@gmail.com>
2024-02-10 23:43:16 -0300

    Inserindo o ScopedModel.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/config/firebase_options.dart';
+import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/pages/home_page.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp(
-    title: 'Flutter´s Clothing',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-      primaryColor: const Color.fromARGB(255, 4, 125, 141),
-      appBarTheme: const AppBarTheme(
-        backgroundColor: Color.fromARGB(255, 4, 125, 141),
-        foregroundColor: Colors.white,
+  runApp(ScopedModel<UserModel>(
+    model: UserModel(),
+    child: MaterialApp(
+      title: 'Flutter´s Clothing',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color.fromARGB(255, 4, 125, 141),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 4, 125, 141),
+          foregroundColor: Colors.white,
+        ),
       ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     ),
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
   ));
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/pages/signup_page.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
@@ -33,71 +35,84 @@ class LoginPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.all(16.0),
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'E-mail'),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text!.isEmpty || !text.contains('@')) {
-                  return 'E-mail inválido!';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Senha'),
-              obscureText: true,
-              validator: (text) {
-                if (text!.isEmpty || text.length < 6) {
-                  return 'Senha inválida.';
-                }
-                return null;
-              },
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  foregroundColor: Theme.of(context).primaryColor,
-                  padding: EdgeInsets.zero,
-                ),
-                child: const Text(
-                  'Esqueci minha senha',
-                  textAlign: TextAlign.right,
-                ),
+      body: ScopedModelDescendant<UserModel>(
+        builder: (BuildContext context, Widget? child, UserModel model) {
+          if (model.isLoading) {
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                    Theme.of(context).primaryColor),
               ),
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              height: 44.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {}
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor,
-                  foregroundColor: Colors.white,
+            );
+          }
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text!.isEmpty || !text.contains('@')) {
+                      return 'E-mail inválido!';
+                    }
+                    return null;
+                  },
                 ),
-                child: const Text(
-                  'Entrar',
-                  style: TextStyle(
-                    fontSize: 18.0,
+                const SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Senha'),
+                  obscureText: true,
+                  validator: (text) {
+                    if (text!.isEmpty || text.length < 6) {
+                      return 'Senha inválida.';
+                    }
+                    return null;
+                  },
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      foregroundColor: Theme.of(context).primaryColor,
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: const Text(
+                      'Esqueci minha senha',
+                      textAlign: TextAlign.right,
+                    ),
                   ),
                 ),
-              ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 44.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                      model.signIn();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
